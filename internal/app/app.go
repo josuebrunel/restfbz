62d385248bb73a8811531609dabe9b774943dc43
Josue Kouka <josuebrunel@gmail.com>
2021-11-25 06:32:59 +0100

    internal/app: add fizzbuzz handler

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,15 +1,21 @@
 package app
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	fbz "restfbz/pkg/fizzbuzz"
+	"strconv"
+	"strings"
 )
 
-// Application is a simple struc holding
-// * router: a simple router
-type Application struct {
-	router *http.ServeMux
+func concat(ss ...string) string {
+	var sb strings.Builder
+	for _, s := range ss {
+		sb.WriteString(s)
+	}
+	return sb.String()
 }
 
 // New : initialize the application
@@ -18,8 +24,55 @@ func New() (app Application) {
 	return
 }
 
+// Application is a simple struc holding
+// * router: a simple router
+type Application struct {
+	router *http.ServeMux
+}
+
 // Run laumches the http server
 func (app Application) Run(port string) {
+	app.routes()
 	log.Printf(fmt.Sprintf("Server started and listening on %s", port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), app.router))
+}
+
+func (app Application) routes() {
+	app.router.HandleFunc("/", app.handleFizzBuzz())
+}
+
+func (app Application) handleFizzBuzz() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Only GET allowed
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		paramsRequired := 5
+		intParams := map[string]int{"int1": -1, "int2": -1, "limit": -1}
+		strParams := map[string]string{"str1": "", "str2": ""}
+
+		for key, values := range r.URL.Query() {
+			if _, ok := intParams[key]; ok {
+				val, err := strconv.Atoi(values[0])
+				if err != nil {
+					http.Error(w, concat("Invalid value for key: ", key), http.StatusBadRequest)
+					return
+				}
+				intParams[key] = val
+			} else {
+				strParams[key] = values[0]
+			}
+			paramsRequired--
+		}
+		if paramsRequired > 0 {
+			http.Error(w, "Invalid number of params", http.StatusBadRequest)
+			return
+		}
+
+		res := fbz.FizzBuzz(intParams["int1"], intParams["int2"], intParams["limit"],
+			strParams["str1"], strParams["str2"])
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(res)
+	}
 }
---
+++ internal/app/app_test.go
@@ -0,0 +1,39 @@
+package app
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestApplication(t *testing.T) {
+	app := New()
+	app.routes()
+	// test with invalid method
+	req := httptest.NewRequest("POST", "/", nil)
+	w := httptest.NewRecorder()
+	app.router.ServeHTTP(w, req)
+	if w.Result().StatusCode != 405 {
+		t.Fatal("Unexpected status code returned")
+	}
+	// test with invalid values
+	req = httptest.NewRequest("GET", "/?int1=3&int2=5&limit=toto&str1=fizz&str2=buzz", nil)
+	w = httptest.NewRecorder()
+	app.router.ServeHTTP(w, req)
+	if w.Result().StatusCode != 400 {
+		t.Fatal("Unexpected status code returned")
+	}
+	// test with wrong numbers of params
+	req = httptest.NewRequest("GET", "/?int1=3&int2=5&limit=25&str1=fizz", nil)
+	w = httptest.NewRecorder()
+	app.router.ServeHTTP(w, req)
+	if w.Result().StatusCode != 400 {
+		t.Fatal("Unexpected status code returned")
+	}
+	// test with a correct request
+	req = httptest.NewRequest("GET", "/?int1=3&int2=5&limit=25&str1=fizz&str2=buzz", nil)
+	w = httptest.NewRecorder()
+	app.router.ServeHTTP(w, req)
+	if w.Result().StatusCode != 200 {
+		t.Fatal("Unexpected status code returned")
+	}
+}
